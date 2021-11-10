c75d4d45c5e64b28e401b4a32d60c3b17a4333c8
Volodymyr Kukytsiak <kykucak@gmail.com>
2021-11-10 10:31:42 +0200

    add comparing

--- main.py
+++ main.py
@@ -55,22 +55,29 @@ def go_to_agency_details_page(agencies_divs):
             return
 
 
-def write_agency_investments_to_excel(writer):
-    print("Start writing agency investments")
-
+def get_investments_rows():
     investments_rows = browser.get_webelements("css:div.dataTables_scrollBody table#investments-table-object tbody tr")
-
-    uiis = [investment_row.find_elements_by_css_selector("td")[0].text for investment_row in investments_rows]
-    bureaus = [investment_row.find_elements_by_css_selector("td")[1].text for investment_row in investments_rows]
-    investment_titles = [investment_row.find_elements_by_css_selector("td")[2].text for investment_row in investments_rows]
-    total_spendings = [investment_row.find_elements_by_css_selector("td")[3].text for investment_row in investments_rows]
-    types = [investment_row.find_elements_by_css_selector("td")[4].text for investment_row in investments_rows]
-    cio_ratings = [investment_row.find_elements_by_css_selector("td")[5].text for investment_row in investments_rows]
-    num_of_projects = [investment_row.find_elements_by_css_selector("td")[6].text for investment_row in investments_rows]
+    return investments_rows
 
 
-    investments_df = get_investments_df(uiis, bureaus, investment_titles, total_spendings, types, cio_ratings, num_of_projects)
+def get_agency_investments_fields():
+        investments_rows = get_investments_rows()
 
+        uiis = [investment_row.find_elements_by_css_selector("td")[0].text for investment_row in investments_rows]
+        bureaus = [investment_row.find_elements_by_css_selector("td")[1].text for investment_row in investments_rows]
+        investment_titles = [investment_row.find_elements_by_css_selector("td")[2].text for investment_row in investments_rows]
+        total_spendings = [investment_row.find_elements_by_css_selector("td")[3].text for investment_row in investments_rows]
+        types = [investment_row.find_elements_by_css_selector("td")[4].text for investment_row in investments_rows]
+        cio_ratings = [investment_row.find_elements_by_css_selector("td")[5].text for investment_row in investments_rows]
+        num_of_projects = [investment_row.find_elements_by_css_selector("td")[6].text for investment_row in investments_rows]
+        return [uiis, bureaus, investment_titles, total_spendings, types, cio_ratings, num_of_projects]
+
+
+def write_agency_investments_to_excel(writer):
+    agency_investments_fields = get_agency_investments_fields()
+
+    print("Start writing agency investments")
+    investments_df = get_investments_df(*agency_investments_fields)
     investments_df.to_excel(writer, sheet_name="Investments", index=False)
     print("Finish writing agency investments")
 
@@ -119,29 +126,78 @@ def download_investment_details_pdf(investment_details_link):
     browser.close_window()
     browser.get_window_handles()
     browser.switch_window("main")
-    print("downloaded pdf")
+    print(f"{investment_details_link.text}.pdf - downloaded")
 
 
 def download_agency_investments_details_pdfs():
     print("Start downloading")
     investments_rows = browser.get_webelements("css:div.dataTables_scrollBody table#investments-table-object tbody tr")
-    investments_rowss = investments_rows[:5]
-    for investment_row in investments_rowss:
+
+    for investment_row in investments_rows:
         investment_details_link = get_investment_details_link(investment_row)
         if investment_details_link is not None:
             download_investment_details_pdf(investment_details_link)
 
 
-def compare_data():
+def compare_investment_titles(investment_title, pdf_investment_title, file_path):
+    if investment_title != pdf_investment_title:
+        print(f"\nInvestment title on the site is not equal to investment title in pdf report in the file - {file_path}.\n")
+    else:
+        print(f"\nInvestment title on the site is equal to investment title in pdf report in the file - {file_path}.\n")
+
+
+def compare_unique_identifiers(uii, pdf_uii, file_path):
+    if uii != pdf_uii:
+        print(f"UII on the site is not equal to UII in pdf report in the file - {file_path}.\n")
+    else:
+        print(f"UII on the site is equal to UII in pdf report in the file - {file_path}.\n")
+
+
+def get_pdf_investment_title(text):
+    start_name_investment = text.find("Name of this Investment:")
+    end_name_investment = text.find("2.")
+
+    pdf_investment_title = text[start_name_investment+25:end_name_investment].strip()
+    return pdf_investment_title
+
+
+def get_pdf_unique_identifier(text):
+    end_name_investment = text.find("2.")
+    end_identifier = text.find("Section B:")
+
+    pdf_unique_identifier = text[end_name_investment+39:end_identifier].strip()
+    return pdf_unique_identifier
+
+
+
+def compare():
+    investments_rows = get_investments_rows()
+    uiis = [investment_row.find_elements_by_css_selector("td")[0].text for investment_row in investments_rows]
+    investment_titles = [investment_row.find_elements_by_css_selector("td")[2].text for investment_row in investments_rows]
+
+
     output_dir_files = os.listdir("./output")
     pdf_file_paths = [file_path for file_path in output_dir_files if ".pdf" in file_path]
+
     for pdf_file_path in pdf_file_paths:
         with open(f"./output/{pdf_file_path}", "rb") as pdf_file:
             pdf_reader = pdf.PdfFileReader(pdf_file)
             first_page = pdf_reader.getPage(0)
             text = first_page.extractText()
-            print(f"text - {text[:20]}\n\n")
 
+            pdf_investment_title = get_pdf_investment_title(text)
+            pdf_unique_identifier = get_pdf_unique_identifier(text)
+
+            for index, uii in enumerate(uiis):
+                if uii == pdf_file_path[:-4]:
+                    compare_investment_titles(investment_titles[index], pdf_investment_title, pdf_file_path)
+                    compare_unique_identifiers(uii, pdf_unique_identifier, pdf_file_path)
+
+                    print(f"site title - {investment_titles[index]}")
+                    print(f"pdf title - {pdf_investment_title}")
+                    print(f"site uii - {uii}")
+                    print(f"pdf uii - {pdf_unique_identifier}\n\n")
+                    break
 
 
 def main():
@@ -152,22 +208,21 @@ def main():
 
         agencies_overviews_divs = get_agencies_overviews_divs()
 
-        # write_agencies_to_excel(writer, agencies_overviews_divs)
+        write_agencies_to_excel(writer, agencies_overviews_divs)
 
         go_to_agency_details_page(agencies_overviews_divs)
 
         show_all_investments_in_table()
 
-        # write_agency_investments_to_excel(writer)
+        write_agency_investments_to_excel(writer)
 
         download_agency_investments_details_pdfs()
 
-        compare_data()
+        compare()
         
         writer.save()
     finally:
         browser.close_all_browsers()
-        print(os.listdir("./output"))
 
 
 if __name__ == "__main__":
